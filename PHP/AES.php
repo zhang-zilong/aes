c8f3be96218014c846de132b245545d93a45cb66
DESKTOP-QVCDHMI\Administrator <zhangzilong@talk-cloud.com>
2020-11-10 16:28:03 +0800

    first commit

---
+++ PHP/AES.php
@@ -0,0 +1,88 @@
+<?php
+class AES {
+	private $iv;
+	private $key;
+	private $bit; //Only can use 128, 256
+	function __construct($key, $bit = 128, $iv = "") {
+		// gen key
+		if($bit == 256){
+			$this->key = hash('SHA256', $key, true);
+		}else{
+			$this->key = hash('MD5', $key, true);
+		}
+
+		// gen iv
+		if($iv != ""){
+			$this->iv = hash('MD5', $iv, true);
+		}else{
+			$this->iv = chr(0).chr(0).chr(0).chr(0).chr(0).chr(0).chr(0).chr(0).chr(0).chr(0).chr(0).chr(0).chr(0).chr(0).chr(0).chr(0); //IV is not set. It doesn't recommend.
+		}
+	}
+
+	function encrypt($str) {
+		if(version_compare(PHP_VERSION,'7.0.0','ge')) {
+			return $this->opensslEncrypt($str);
+		} else {
+			return $this->mcryptEncrypt($str);
+		}
+	}
+
+	function decrypt($str) {
+		if(version_compare(PHP_VERSION,'7.0.0','ge')) {
+			return $this->opensslDecrypt($str);
+		} else {
+			return $this->mcryptDecrypt($str);
+		}
+	}
+
+	private function opensslEncrypt($str) {
+		$data = openssl_encrypt($str, 'AES-128-CBC', $this->key, OPENSSL_RAW_DATA, $this->iv);
+		return base64_encode($data);
+	}
+
+	private function opensslDecrypt($str) {
+		$decrypted = openssl_decrypt(base64_decode($str), 'AES-128-CBC', $this->key, OPENSSL_RAW_DATA, $this->iv);
+		return $decrypted;
+	}
+	
+	private function mcryptEncrypt($str) {
+		//Open
+		$module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+		mcrypt_generic_init($module, $this->key, $this->iv);
+
+		//Padding
+		$block = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC); //Get Block Size
+		$pad = $block - (strlen($str) % $block); //Compute how many characters need to pad
+		$str .= str_repeat(chr($pad), $pad); // After pad, the str length must be equal to block or its integer multiples
+
+		//Encrypt
+		$encrypted = mcrypt_generic($module, $str);
+
+		//Close
+		mcrypt_generic_deinit($module);
+		mcrypt_module_close($module);
+
+		//Return
+		return base64_encode($encrypted);
+	}
+
+	private function mcryptDecrypt($str) {
+		//Open 
+		$module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+		mcrypt_generic_init($module, $this->key, $this->iv);
+
+		//Decrypt
+		$str = mdecrypt_generic($module, base64_decode($str)); //Get original str
+
+		//Close
+		mcrypt_generic_deinit($module);
+		mcrypt_module_close($module);
+
+		//Depadding
+		$slast = ord(substr($str, -1)); //pad value and pad count
+		$str = substr($str, 0, strlen($str) - $slast);
+
+		//Return
+		return $str;           
+	}
+}
---
+++ README.md
@@ -0,0 +1,41 @@
+# EasyAES
+AES encrypt/decrypt, Android, iOS, php compatible(兼容php, Android, iOS平台) 
+
+可以在三个平台间加密解密数据，确保某个平台加密的数据，在另外两个平台能够解密。
+
+## Android版本用法：
+```Java
+String text = "this is pliat text.";
+// encrypt
+String data = EasyAES.encryptString(data);
+// dencrypt
+String plaitText = EasyAES.dencryptString(data);
+```
+
+
+## php版本用法(兼容php7.x)：
+```PHP
+function encryptString($content) {
+	$aes = new AES('****************', 128, '################');
+	return $aes->encrypt($content);
+}
+ 
+function decryptString($content) {
+	$aes = new AES('****************', 128, '################');
+	return $aes->decrypt($content);
+}
+
+需要在类库中设置    key 和 iv
+
+```
+注意：php7.0以下版本用到了mcrypt模块，需要安装并在php.ini中开启。7.0及以上版本需要ssl模块，无需mcrypt
+
+
+## iOS版本用法
+```Object-C
+NSData* pData = ...//encrypted data form server
+NSData* plaitData = [NSData AES128DecryptedData:data];
+```
+
+
+以上所有版本都需要设置下自己的加密密码以及偏移向量iv，均为16位字符。
